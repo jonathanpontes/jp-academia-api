dfc591ef37c53abfb0e3a45d566c1bd05afb7cb8
Jonathan Pontes <jonathan.pontes@hotmail.com>
2021-11-12 16:53:56 -0300

    inclusão de tratamento de cors

--- src/main/java/com/academia/adapters/configurations/CorsConfiguration.java
+++ src/main/java/com/academia/adapters/configurations/CorsConfiguration.java
@@ -1,5 +1,7 @@
 package com.academia.adapters.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Value("${origem.front}")
+    private String origemPermitida;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins(origemPermitida)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
